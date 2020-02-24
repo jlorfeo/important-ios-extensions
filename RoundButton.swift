ba7e8fcfbe10368cdb85f1d082f7555e2f7850e3
Jan-Erik LORFEO <u074619@canda.com>
2020-02-24 20:53:07 +0100

    Add RoundButton extension

---
+++ RoundButton.swift
@@ -0,0 +1,42 @@
+//
+//  RoundButton.swift
+//
+//  Found in: https://stackoverflow.com/questions/26961274/how-can-i-make-a-button-have-a-rounded-border-in-swift
+//  Created by Jan-Erik LORFEO on 24.02.20.
+//  Copyright © 2020 jel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
